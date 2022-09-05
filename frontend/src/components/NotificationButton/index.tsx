fc4c16b710842e3b4a005c212befb9b7a5aaff41
karlissonJR <karlissonj@gmail.com>
2022-09-05 16:58:28 -0300

    card de vendas com a tabela

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -5,10 +5,8 @@ import './styles.css'
 function NotificationButton() {
 
     return (
-        <div className="dsmeta-red-btn-container">
-            <div className="dsmeta-red-btn">
-                <img src={notificationIcon} alt="notificar" />
-            </div>
+        <div className="dsmeta-red-btn">
+            <img src={notificationIcon} alt="notificar" />
         </div>
     )
 }
