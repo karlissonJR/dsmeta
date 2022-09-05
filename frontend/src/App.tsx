daabb489fd231dea47af7f9eb25d3fa78d2351e3
karlissonJR <karlissonj@gmail.com>
2022-09-05 16:23:58 -0300

    NotificationButton and Header components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
-
+import Header from "./components/Header"
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
 
   return (
-    <h1>OLA</h1>
+    <>
+      <Header />
+      <NotificationButton />
+    </>
   )
 }
 
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import logo from '../../assets/img/logo.svg'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="logo" />
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por <a href="https://github.com/karlissonJR" target="_blank">@karlissonJR</a></p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
