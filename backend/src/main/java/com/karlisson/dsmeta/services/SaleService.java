e3773baae1b591eddb09ecc52e42a765f362a2d7
karlissonJR <karlissonj@gmail.com>
2022-09-07 10:27:11 -0300

    datas nos parametros da url de vendas

--- backend/src/main/java/com/karlisson/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/karlisson/dsmeta/services/SaleService.java
@@ -3,8 +3,11 @@ package com.karlisson.dsmeta.services;
 import com.karlisson.dsmeta.entities.Sale;
 import com.karlisson.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -13,8 +16,12 @@ public class SaleService {
     @Autowired
     private SaleRepository repository;
 
-    public List<Sale> findSales() {
-        return repository.findAll();
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+        LocalDate min = LocalDate.parse(minDate);
+        LocalDate max = LocalDate.parse(maxDate);
+
+        return repository.findSales(min, max, pageable);
     }
 
 }
