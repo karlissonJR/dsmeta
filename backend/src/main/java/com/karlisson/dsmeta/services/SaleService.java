bea89412197f3859110a21ce5dc5c32ffecd1e38
karlissonJR <karlissonj@gmail.com>
2022-09-07 09:47:46 -0300

    add list service to sales

---
+++ backend/src/main/java/com/karlisson/dsmeta/services/SaleService.java
@@ -0,0 +1,20 @@
+package com.karlisson.dsmeta.services;
+
+import com.karlisson.dsmeta.entities.Sale;
+import com.karlisson.dsmeta.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository repository;
+
+    public List<Sale> findSales() {
+        return repository.findAll();
+    }
+
+}
